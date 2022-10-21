2e29b0f8a3007d032b58e692371c041ac0c16a10
kris-randen <kris.randen@gmail.com>
2022-10-21 11:12:35 +0530

    Xcode project created.

---
+++ messaging/messaging/messagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  messagingApp.swift
+//  messaging
+//
+//  Created by Krishnaswami Rajendren on 10/21/22.
+//
+
+import SwiftUI
+
+@main
+struct messagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
